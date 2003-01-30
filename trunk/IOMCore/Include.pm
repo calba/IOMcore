2f82173f5c600b46c8512d6cb5f1e74d72f7d17e
CAP@HOME <calba@tid.es>
2003-01-30 12:01:56 +0100

    [svn r10] Añadida documentacion en POD
    --HG--
    branch : iomcore

--- trunk/IOMCore/Include.pm
+++ trunk/IOMCore/Include.pm
@@ -40,3 +40,29 @@ sub Include($)
 
 1;
   
+=head1 NAME
+
+IOMCore::Include - Lee un fichero completo y lo almacena en una cadena
+
+=head1 SYNOPSIS
+
+    use IOMCore::Include;
+
+    $mifichero=Include("Fichero");
+
+=head1 DESCRIPCION
+
+Este modulo carga un fichero y lo almacena en memoria. No se hace ningun tipo de procesado.
+
+=head1 Version
+
+$Id: Include.pm,v 1.2 2003-01-30 11:01:56 calba Exp $
+
+=head1 Cambios
+
+$Log: not supported by cvs2svn $
+
+=cut
+
+
+
