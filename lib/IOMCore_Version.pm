aefce487829102b2bab8293faa9e7d74d3f8e151
cap@CASA <cesar.alba@gmail.com>
2016-12-10 11:15:33 +0100

    Añadido IOMCore::IPaddress

--- lib/IOMCore_Version.pm
+++ lib/IOMCore_Version.pm
@@ -8,12 +8,12 @@ BEGIN {
   #$Exporter::Verbose=1;
   our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
   # if using RCS/CVS, this may be preferred
-  $VERSION = '1.0';
+  $VERSION = '1.1';
   @ISA         = qw(Exporter);
   @EXPORT      = qw();
   # as well as any optionally exported functions
   @EXPORT_OK   = qw();
    }
-  
+
 
 1;
